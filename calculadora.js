e7e74965fa799394b432e64d33ec3ef399562ec5
matshw <mateusdallacourt699@gmail.com>
2024-04-22 21:35:21 -0400

    primeiro commit

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+function adicao(v1, v2){
+    return v1 + v2;
+}
+
+function subtracao(valor1, valor2){
+    return valor1 - valor2;
+}
+
+function multiplicacao(valor1, valor2){
+    return valor1 * valor2;
+}
+
+function divisao(valor1, valor2){
+    return valor1 / valor2;
+}
+
+module.exports = {
+    adicao: adicao,
+    subtracao: subtracao,
+    multiplicacao: multiplicacao,
+    divisao: divisao
+};
